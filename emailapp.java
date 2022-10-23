ff5a78b12cd15f1c8e6faf87d041cf9e37f8242b
Mohan Ma <112891561+Joseph941230@users.noreply.github.com>
2022-10-23 17:34:58 -0400

    Add files via upload
    oop Email

---
+++ emailapp.java
@@ -0,0 +1,12 @@
+public class emailapp {
+
+
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Joe", "Ma");
+        System.out.println(em1.shoInfo());
+
+       // em1.setAlternateEmail("sdfnfjn@gmail.com");
+        //System.out.println("Your person email is :" + em1.getAlternateEmail());
+    }
+}
